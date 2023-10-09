203d1444252ead3fce0ad49a1a23c05042dce5fe
Martti Mourujärvi <marttimourujarvi@gmail.com>
2023-10-09 21:27:58 +0300

    add sounds to shooting

--- shooting.js
+++ shooting.js
@@ -3,11 +3,26 @@ import { aimCircles } from './game_logic';
 
 const raycaster = new THREE.Raycaster();
 
+const shootSound = document.getElementById("shoot1");
+
+let currentAudio = false;
+
+function playAudio(audioElement) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+    currentAudio = audioElement;
+    audioElement.play();
+};
+
 export function fire(camera, scene) {
     const origin = camera.position;
     const direction = camera.getWorldDirection(new THREE.Vector3());
 
     raycaster.set(origin, direction);
+    
+    playAudio(shootSound);
 
     const intersects = raycaster.intersectObjects(scene.children);
     const object_ids = intersects.map(intersect => intersect.object.uuid);
@@ -16,7 +31,6 @@ export function fire(camera, scene) {
         aimCircles.forEach(circle => {
             //console.log(circle)
             if (object_ids.includes(circle.mesh.uuid)) {
-                console.log("osui")
                 scene.remove(circle.mesh);
                 circle.mesh.geometry.dispose();
                 circle.mesh.material.dispose();
